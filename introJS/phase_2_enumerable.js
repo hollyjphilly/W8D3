3d5c68787ef34260f6fc92dd76def74984920a3d
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-02-17 17:05:04 -0500

    Array#myReduce

--- introJS/phase_2_enumerable.js
+++ introJS/phase_2_enumerable.js
@@ -1,7 +1,45 @@
 Array.prototype.myEach = function(callback) {
 
     for (let i = 0; i < this.length; i++) {
-
-        callback(this[i])
+        callback(this[i], i)
     };
+
 };
+
+
+Array.prototype.myMap = function(callback) {
+    let new_arr = []
+
+    this.myEach( function (el, i) {
+        new_arr.push(callback(el))
+    });
+
+    return new_arr
+    // console.log(new_arr) 
+
+};
+
+
+Array.prototype.myReduce = function(callback, initialValue) {
+    if (initialValue === undefined) {
+        let acc = this[0]
+
+        this.slice(1).myEach(function (el, i) {
+            acc = callback(acc, el)
+        });
+        return acc
+    } else {
+        let acc = initialValue
+
+        this.myEach(function (el, i) {
+            acc = callback(acc, el)
+        });
+        return acc
+    }
+}
+
+
+
+
+
+
